99487a530f2aeea1569133c1e14caa2577f36332
Mehmet Anil Inec <83233804+anilinec@users.noreply.github.com>
2022-04-27 15:14:17 -0700

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Welcome } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Welcome />
+    </div>
+
+  </div>
+);
+
+export default App;
